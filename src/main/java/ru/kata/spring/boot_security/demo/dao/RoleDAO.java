f0e5439e47f3e39da50a3767fe951745c3301766
Artem Baykov <baikoff@bk.ru>
2022-08-04 17:48:16 +0300

    kak i v 3_1_2

--- src/main/java/ru/kata/spring/boot_security/demo/dao/RoleDAO.java
+++ src/main/java/ru/kata/spring/boot_security/demo/dao/RoleDAO.java
@@ -1,14 +1,8 @@
 package ru.kata.spring.boot_security.demo.dao;
 
-import ru.kata.spring.boot_security.demo.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.kata.spring.boot_security.demo.model.User;
 
-import java.util.List;
-import java.util.Set;
+public interface RoleDAO extends JpaRepository<User, Long> {
 
-public interface RoleDAO {
-     List<Role> allRoles();
-
-     Role getRole(Long id);
-
-     Set<Role> setRole(Long index);
 }
